38d646ecfdf613c2e9e7bb59997421beeac31bd4
benjamin.mrl <benjamin.mrl@gmail.com>
2023-02-22 08:39:31 +0100

    Final modif

--- slackAPI/api.py
+++ slackAPI/api.py
@@ -1,107 +1,49 @@
 import pickle
-import requests
 import warnings
-import json
+import shap
 
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
 from flask import Flask, request, jsonify
-from sklearn.neighbors import NearestNeighbors
+#from sklearn.neighbors import NearestNeighbors
 warnings.filterwarnings("ignore")
-# app = pd.read_csv("app_encoded.csv", sep=',',
-#                   index_col=0, encoding='utf8')
-# # app_no_encoded = pd.read_csv("app_no_enconded_featureengineering.csv")
-# app20 = app
-# # # Divise app en jeu test et entrainement
-# # train_set = app20.loc[~app20['Test'], :]
-# test_set = app20.loc[app20['Test'], :]
-
-# # Ensure all data is stored as floats
-# train_set = train_set.astype(np.float64)
-# train_set.drop(columns=["Test"], inplace=True)
-# test_set = test_set.astype(np.float64)
-# test_set.drop(columns=["Test"], inplace=True)
-
-# # Target labels
-# y_train = train_set['TARGET']
-# y_test = test_set["TARGET"]
-
-# # Remove test/train indicator column and target column
-# x_train = train_set.drop(columns=['TARGET'])
-# x_test = test_set.drop(columns=['TARGET'])
 
 app = Flask(__name__)
 
 with open('banking_model20.md', 'rb') as f:
-    pickled_model = pickle.load(f)
+    banking_model = pickle.load(f)
+
+with open('Knn.md', 'rb') as f:
+    Knn_model = pickle.load(f)
+
+with open('Explainer.md', 'rb') as f:
+    explainer = pickle.load(f)
+
+def plot_shap_bar(sv, name='../image/test.png',
+                  adjust_left=0.35):
+    fig = plt.figure()
+    shap.plots.bar(sv[0], show=False)
+    plt.gcf().set_size_inches(15,6)
+    fig.subplots_adjust(left=adjust_left)
+    plt.savefig(name)
 
 
 @app.route('/solvabilite',methods=['POST','GET'])
 def solvabilite():
     data=request.json
     data=pd.DataFrame(data, index=[0])
-    data.columns = ['CREDIT_TO_ANNUITY_RATIO', 'EXT_SOURCE_1', 'EXT_SOURCE_2',
-                    'EXT_SOURCE_3', 'DAYS_BIRTH', 'AMT_ANNUITY',
-                    'MEDIAN(payments.AMT_PAYMENT)', 'AMT_GOODS_PRICE',
-                    'DAYS_ID_PUBLISH', 'COUNT(bureau)', 'COUNT(cash_balance)',
-                    'MEDIAN(cash_balance.CNT_INSTALMENT_FUTURE)', 'AMT_CREDIT',
-                    'MEDIAN(prev_app.CNT_PAYMENT)',
-                    'MEDIAN(bureau.DAYS_CREDIT_ENDDATE)', 'CODE_GENDER',
-                    'DAYS_EMPLOYED', 'MEDIAN(bureau.AMT_CREDIT_SUM)',
-                    'MEDIAN(bureau.AMT_CREDIT_MAX_OVERDUE)', 'NAME_EDUCATION_TYPE']
 
-   
-    with open('banking_model20.md', 'rb') as f:
-        try:
-            pickled_model = pickle.load(f)
-        except:
-            print('error')
-    app20= pd.read_csv("app_encoded.csv", sep=',',
-                      index_col=0, encoding='utf8')
-    test_set = app20.loc[app20['Test'], :]
-    test_set = test_set[['CREDIT_TO_ANNUITY_RATIO', 'EXT_SOURCE_1', 'EXT_SOURCE_2',
-           'EXT_SOURCE_3', 'DAYS_BIRTH', 'AMT_ANNUITY',
-           'MEDIAN(payments.AMT_PAYMENT)', 'AMT_GOODS_PRICE',
-           'DAYS_ID_PUBLISH', 'COUNT(bureau)', 'COUNT(cash_balance)',
-           'MEDIAN(cash_balance.CNT_INSTALMENT_FUTURE)', 'AMT_CREDIT',
-           'MEDIAN(prev_app.CNT_PAYMENT)',
-           'MEDIAN(bureau.DAYS_CREDIT_ENDDATE)', 'CODE_GENDER',
-           'DAYS_EMPLOYED', 'MEDIAN(bureau.AMT_CREDIT_SUM)',
-           'MEDIAN(bureau.AMT_CREDIT_MAX_OVERDUE)', 'NAME_EDUCATION_TYPE']]
-    test_set
-    df = test_set.iloc[:3,]
-    pickled_model.predict(df)
-    #solvable=pickled_model.predict(data)
+    result = dict()
+    result['predict_proba'] = banking_model.predict_proba(data).tolist()[0]
+    result['predict'] = banking_model.predict(data).tolist()
+    
+    shap_value = explainer(data, check_additivity=False)
+    plot_shap_bar(shap_value)
 
-    print(data)
-    return "ok"
-    jsonify(solvable.tolist())
-    # Transformation du jeu test
-    x_test_transformed = pd.DataFrame(clf_0[0].transform(x_test),
-                          columns=x_test.columns,
-                          index=x_test.index)
+    return jsonify(result)
 
-    nbrs = NearestNeighbors(n_neighbors=10, algorithm='ball_tree').fit(x_test_transformed)
-
-    # On recupere l'indice des plus proches voisins du client  a tester
-    indices = nbrs.kneighbors(x_test_transformed[0:1])[nbrs].flatten()
-    app_no_encoded.iloc[indices]
-    lime1 = LimeTabularExplainer(x_test_transformed,
-                             feature_names=x_test.columns,
-                             class_names=["Solvable", "Non Solvable"],
-                             discretize_continuous=False)
-                            
-
-    exp = lime1.explain_instance(x_test_transformed.iloc[100],
-                             clf_0.predict_proba,
-                             num_samples=100)
-    # Id client
-    x_test_transformed.index["index du premier NN"]
-    exp.show_in_notebook(show_table=False)
-
-    exp.as_pyplot_figure()
-    plt.tight_layout()
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
-    #print(model)
--- streamliteapp/main.py
+++ streamliteapp/main.py
@@ -1,6 +1,4 @@
-import time
 import datetime
-import json
 import requests
 
 import streamlit as st
@@ -21,30 +19,29 @@ with tab1:
     title = st.text_input('Adresse', 'Adresse')
     title = st.text_input('CP', 'Code Postal')
     title = st.text_input('Ville', 'Ville')
-    date_naissance = st.date_input(
-    "Date de naissance",
-    datetime.date(2001, 11, 9))
+    date_naissance = st.date_input("Date de naissance",
+                                   datetime.date(2001, 11, 9))
 
-    data1 = st.number_input('data1')
-    data2 = st.number_input('data2')
-    data3 = st.number_input('data3')
-    data4 = st.number_input('data4')
-    data5 = st.number_input('data5')
-    data6 = st.number_input('data6')
-    data7 = st.number_input('data7')
-    data8 = st.number_input('data8')
-    data9 = st.number_input('data9')
-    data10 = st.number_input('data10')
-    data11 = st.number_input('data11')
-    data12 = st.number_input('data12')
-    data13 = st.number_input('data13')
-    data14 = st.number_input('data14')
-    data15 = st.number_input('data15')
-    data16 = st.number_input('data16')
-    data17 = st.number_input('data17')
-    data18 = st.number_input('data18')
-    data19 = st.number_input('data19')
-    data20 = st.number_input('data20')
+    data1 = st.number_input('CREDIT_TO_ANNUITY_RATIO', value=0.416986)
+    data2 = st.number_input('EXT_SOURCE_1', value=0.0)
+    data3 = st.number_input('EXT_SOURCE_2', value=-0.38)
+    data4 = st.number_input('EXT_SOURCE_3', value=0.0)
+    data5 = st.number_input('DAYS_BIRTH', value=-0.689503)
+    data6 = st.number_input('AMT_ANNUITY', value=0.128735)
+    data7 = st.number_input('MEDIAN(payments.AMT_PAYMENT)', value=-0.465475)
+    data8 = st.number_input('AMT_GOODS_PRICE', value=0.510204)
+    data9 = st.number_input('DAYS_ID_PUBLISH', value=-0.353238)
+    data10 = st.number_input('COUNT(bureau)', value=0.166667)
+    data11 = st.number_input('COUNT(cash_balance)', value=-0.037037)
+    data12 = st.number_input('MEDIAN(cash_balance.CNT_INSTALMENT_FUTURE)', value=0.444444)
+    data13 = st.number_input('AMT_CREDIT', value=0.299766)
+    data14 = st.number_input('MEDIAN(prev_app.CNT_PAYMENT)', value=0.0)
+    data15 = st.number_input('MEDIAN(bureau.DAYS_CREDIT_ENDDATE)', value=-1.905672)
+    data16 = st.number_input('CODE_GENDER', value=1.0)
+    data17 = st.number_input('DAYS_EMPLOYED', value=-1.265463)
+    data18 = st.number_input('MEDIAN(bureau.AMT_CREDIT_SUM)', value=-0.423168)
+    data19 = st.number_input('MEDIAN(bureau.AMT_CREDIT_MAX_OVERDUE)', value=31.5)
+    data20 = st.number_input('NAME_EDUCATION_TYPE', value=0.0)
 
 with st.form("my_form"):
     st.write("Soumettre le dossier")
@@ -54,33 +51,30 @@ with st.form("my_form"):
     if submitted:
         # st.write("slider", slider_val, "checkbox", checkbox_val)
         valeuratester = {
-        'data1': data1,
-        'data2': data2,
-        'data3': data3,
-        'data4': data4,
-        'data5': data5,
-        'data6': data6,
-        'data7': data7,
-        'data8': data8,
-        'data9': data9,
-        'data10': data10,
-        'data11': data11,
-        'data12': data12,
-        'data13': data13,
-        'data14': data14,
-        'data15': data15,
-        'data16': data16,
-        'data17': data17,
-        'data18': data18,
-        'data19': data19,
-        'data20': data20,
+        'CREDIT_TO_ANNUITY_RATIO': data1,
+        'EXT_SOURCE_1': data2,
+        'EXT_SOURCE_2': data3,
+        'EXT_SOURCE_3': data4,
+        'DAYS_BIRTH': data5,
+        'AMT_ANNUITY': data6,
+        'MEDIAN(payments.AMT_PAYMENT)': data7,
+        'AMT_GOODS_PRICE': data8,
+        'DAYS_ID_PUBLISH': data9,
+        'COUNT(bureau)': data10,
+        'COUNT(cash_balance)': data11,
+        'MEDIAN(cash_balance.CNT_INSTALMENT_FUTURE)': data12,
+        'AMT_CREDIT': data13,
+        'MEDIAN(prev_app.CNT_PAYMENT)': data14,
+        'MEDIAN(bureau.DAYS_CREDIT_ENDDATE)': data15,
+        'CODE_GENDER': data16,
+        'DAYS_EMPLOYED': data17,
+        'MEDIAN(bureau.AMT_CREDIT_SUM)': data18,
+        'MEDIAN(bureau.AMT_CREDIT_MAX_OVERDUE)': data19,
+        'NAME_EDUCATION_TYPE': data20,
         }
         r = requests.get('http://127.0.0.1:5002/solvabilite', json = valeuratester)
         solvable = r.json()
-        print(r)
-        if r.text != 'ok':
-            print('sovlvable')
-        else:
-            print('non solvable')
+        print(solvable)
+ 
 with tab2:
-    st.image('PAD.png')
+    st.image('../image/test.png')
